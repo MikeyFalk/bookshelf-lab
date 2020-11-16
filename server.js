527152ba39c1aae0ca4f74f5ff1b68b0cb7e3d26
Michael Falk <falk478@yahoo.com>
2020-11-16 15:11:33 -0800

    spun up server

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const superagent = require('superagent');
+const PORT = process.env.PORT || 3333;
+
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', renderHome);
+app.get('/searches/new', showForm);
+app.post('/searches', createSearch);
+
+function renderHome(req, res) {
+    res.render('pages/index');
+}
+
+function showForm(req, res) {
+    res.render('pages/searches/new.ejs');
+}
+
+function createSearch(req, res) {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+    if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
+    if (req.body.search[1] === 'author') { url += `inauthor:${req.body.search[0]}`; }
+
+    superagent.get(url)
+        .then(data => {
+            return data.body.items.map(book => {
+                return new book(book.volumeInfo);
+            });
+        })
+        .then(results => {
+            res.render('pages/show', { searchResults: JSON.stringify(results) });
+        })
+        .catch(err => console.error(err));
+}
+
+function Book(info) {
+    this.title = info.title || 'no title available';
+}
+
+
+app.listen(PORT, () => {
+    console.log(`server is running:::: ${PORT}`);
+});
